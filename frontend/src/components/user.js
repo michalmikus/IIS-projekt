5dd83f950b6268d763ceb7d5693da9b61fe58140
Žaneta Grossová <xgross11@vutbr.cz>
2021-11-28 23:40:45 +0100

    user detail

--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -4,11 +4,11 @@ const User = ( {user} ) => {
     return (
 
         <div className = "user">
-            <h3>{user.name} {user.surname}</h3>
-            <h4> {user.address} </h4>
-            <h4> {user.telephone} </h4>
-            <h4> {user.mail} </h4>
-            <h4> {user.country} </h4>
+            <h3>{user.address.name} {user.address.surname}</h3>
+            <h4> {user.address.address} </h4>
+            <h4> {user.phoneNumber} </h4>
+            <h4> {user.email} </h4>
+            <h4> {user.address.country} </h4>
         </div>
     );
 }
