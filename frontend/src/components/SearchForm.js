85a22421f2536f5164d182334116b7df87ab0fc9
Žaneta Grossová <xgross11@vutbr.cz>
2021-11-28 15:33:17 +0100

    receiving json from server

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,72 +1,50 @@
 import React from 'react'
 import Button from './Button'
-import { useState } from "react";
-
-const JSON_source = [
-    {
-        "id": "3bef2bfc-188f-42bb-b6f7-08d9b0009d65",
-        "name": "Trencin-Brno"
-    },
-    {
-        "id": "0cbada36-225b-4be7-b6f8-08d9b0009d65",
-        "name": "Brno-Trencin"
-    },
-    {
-        "id": "0b15d7b0-c469-4551-49a2-08d9b00167bc",
-        "name": "Praha-Brno"
-    },
-    {
-        "id": "c36e3551-aa0b-428f-49a3-08d9b00167bc",
-        "name": "Brno-Praha"
-    },
-    {
-        "id": "d3290cf0-4418-4ad1-941a-08d9b009f664",
-        "name": "Praha-Brno"
-    },
-    {
-        "id": "02c714d7-5bd0-4b07-ee24-08d9b02fb78a",
-        "name": "Praha-Brno"
-    },
-    {
-        "id": "7942f93e-87a8-4f0f-6b31-08d9b101ea58",
-        "name": "Zlin-Brno"
-    },
-    {
-        "id": "68975181-baa8-4f99-6b32-08d9b101ea58",
-        "name": "Brno-Zlin"
-    },
-    {
-        "id": "5fe79d26-0d26-4fb8-7bb4-08d9b10732cd",
-        "name": "Brno Kr. Pole - Hlavas"
-    },
-    {
-        "id": "66d73f98-e146-4acf-0f1c-08d9b1ca6c20",
-        "name": "Zlin-Praha"
-    }
-]
+import { useState, useEffect } from "react";
 
 const SearchForm = () => {
 
     const [selectedID, setSelects]=useState();
     const [selectedTime, setTime]=useState();
-
-
-    const requestOptions = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-        };
+    const [connection, setConnection]=useState(null);
 
     const path = 'https://localhost:7293/api/home';
 
-    const res = fetch(path, requestOptions)
+    const getResult = async () => {
+        try {
+            const res = await fetch(path);
+            const datas = await res.json();
+            console.log("error:", datas);
+            setConnection(datas);
+        }
+        catch (error) {
+            console.log("error:", error);
+        }
+    };
 
-    .then(res => {
-    console.log("response: ", res.body);
+    useEffect(() => {
+        getResult();
+    }, []);
+        
+/*
+        .then(res => {
+            console.log("response: ", res.json().then(data =>{
+                JSON_connections = JSON.stringify(data);
+            }));
+            //console.log("response: ", JSON.stringify(res.json()));
+             //   
+            })
+        
+        .catch(err => {
+        console.log("error:", err);
+        });  
 
-    })
-    .catch(err => {
-    console.log("error:", err);
-    });  
+        return JSON_connections;
+    }
+
+    console.log("JSON_connections: ", JSON_connections);
+
+    JSON_source = getResult();*/
 
     const searchConnections = () => {
         const requestOptions = {
@@ -98,8 +76,6 @@ const SearchForm = () => {
 
         }
     }
-
-
     
     return (
         
@@ -109,10 +85,11 @@ const SearchForm = () => {
 
             <h2>{selectedID}</h2>
 
-            <select class="item" value={selectedID} onChange={e=>setSelects(e.target.value)}>{
-                JSON_source.map((result)=>(<option value = {result.id} >{result.name}</option>))
-            }</select>
-            
+            {connection && (
+                <select class="item" value={selectedID} onChange={e=>setSelects(e.target.value)}>{connection.map((result) => (<option value = {result.id}>{result.name}</option>))}
+                </select>
+            )}
+
             </label>
 
             <input type="time" id="time" name="time" class="item" value={selectedTime} onChange={e=>setTime(e.target.value)}></input>
